08338de2f7e0758247cce0994918886d4e0edbb5
Andyintae <runrunintae@gmail.com>
2020-04-30 12:06:15 +0900

    casheSolve_try_1

--- cachematrix.R
+++ cachematrix.R
@@ -6,21 +6,32 @@ makeCacheMatrix <- function(x = matrix()) {
       set <- function(y) {
         x <<- y
         m <<- NULL
+      }
         get <- function() x
         setinverse <- function(inverse)m <<- inverse
-        
-      }
-  
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
+z <- makeCacheMatrix(x)
 
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- inverse(data, ...)
+    x$setinverse(m)
+    m
 }
 
+cacheSolve(z)
 
 ## Example: Caching the Mean of a Vector 
 
